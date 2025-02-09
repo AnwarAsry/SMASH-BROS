81f8d3c33a1e9b3e6989a45f03ca93f74b3ca271
AnwarAsry <anwareducation03@gmail.com>
2025-02-09 20:44:00 +0100

    fixed

--- src/services/characterService.ts
+++ src/services/characterService.ts
@@ -3,7 +3,7 @@ import { ICharacter } from "../models/ICharacter";
 import { get } from "./serviceBase";
 
 export const getAllCharacters = async (sortBy: string | "id", sortOrder: string | "asc"): Promise<ICharacter[]> => {
-    const url = `http://localhost:3000/character?sortBy=${sortBy}&sortOrder=${sortOrder}`
+    const url = `http://localhost:3000/character?sortBy=${sortBy}&sortOrder=${sortOrder}&q=`
 
     const data = await get<IAPIResponse>(url);
     return data.rows;
