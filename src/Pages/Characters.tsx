31e1c804b5001218d1fa40de3208a506cf6133c7
AnwarAsry <anwareducation03@gmail.com>
2025-02-12 21:54:46 +0100

    Removed top picks

--- src/Pages/Characters.tsx
+++ src/Pages/Characters.tsx
@@ -3,9 +3,30 @@ import Gridlayout from "@/styles/GridLayout.module.scss"
 
 import { SearchForm } from "@/components/Form/SearchForm"
 import { GridLayout } from "@/components/GridLayout"
+import { ICharacter } from "@/models/ICharacter";
+import { getAllCharacters } from "@/services/characterService";
+import { useState, useEffect } from "react";
+import { CharacterCard } from "@/components/Characters/CharacterCard";
 
 
 function Characters() {
+  const [characters, setCharacters] = useState<ICharacter[]>([]);
+
+  const sortOrder: string = "asc"
+  const sortBy: string = "id"
+
+  const addCharactersToState = async () => {
+    try {
+      const rows = await getAllCharacters(sortBy, sortOrder)
+      setCharacters(rows)
+    } catch (error) {
+      console.log("[FETCH ERROR] Could not fetch data:", error);
+    }
+  }
+
+  useEffect(() => {
+    addCharactersToState()
+  }, [])
 
   return (
     <>
@@ -15,9 +36,9 @@ function Characters() {
         </section>
         <section>
           <GridLayout className={Gridlayout.AllCharactersGrid}>
-            {/* {
+            {
               characters?.map((char) => <CharacterCard character={char} key={char.id} />)
-            } */}
+            }
           </GridLayout>
         </section>
       </main>
--- src/Pages/Landing.tsx
+++ src/Pages/Landing.tsx
@@ -1,37 +1,11 @@
 import '@/App.scss'
 import IntroStyles from "@/styles/Introduction.module.scss"
 import ButtonStyles from "@/styles/Buttons.module.scss"
-import Gridlayout from "@/styles/GridLayout.module.scss"
 
-import { ICharacter } from '@/models/ICharacter.ts';
-import { GridLayout } from "@/components/GridLayout"
-import { CharacterCard } from "@/components/Characters/CharacterCard"
 import { SearchForm } from "@/components/Form/SearchForm"
-
-import { getAllCharacters } from '@/services/characterService.ts';
-
-import { useEffect, useState } from 'react'
 
 
 const LandingPage = () => {
-  const [characters, setCharacters] = useState<ICharacter[]>([]);
-
-  const sortOrder: string = "asc"
-  const sortBy: string = "id"
-
-  const addCharactersToState = async () => {
-    try {
-      const rows = await getAllCharacters(sortBy, sortOrder)
-      setCharacters(rows)
-    } catch (error) {
-      console.log("[FETCH ERROR] Could not fetch data:", error);
-    }
-  }
-
-  useEffect(() => {
-    addCharactersToState()
-  }, [])
-
   return <>
     <section className={IntroStyles.IntroWrapper}>
       <div className={IntroStyles.IntroTitle}>
@@ -44,14 +18,6 @@ const LandingPage = () => {
           <button className={ButtonStyles.PrimaryBtn}>Spirits</button>
         </div>
       </div>
-    </section>
-    <section>
-      <h2 className={IntroStyles.TopPlayersTitle}>Top Players</h2>
-      <GridLayout className={Gridlayout.TopPicksGrid}>
-        {
-          characters.map((char) => <CharacterCard character={char} key={char.id} />)
-        }
-      </GridLayout>
     </section>
   </>
 }
